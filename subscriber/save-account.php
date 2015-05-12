9676e909f72949ca06f347be2fd5ec7a83dbd747
jeromeangeles <jerome.angeles70@gmail.com>
2015-05-12 18:33:37 +0800

    added security question and answer

--- subscriber/save-account.php
+++ subscriber/save-account.php
@@ -1,6 +1,7 @@
 <?php
 	require_once '../session.php';
 	include_once '../controller/User.Controller.php';
+	require_once '../controller/Security.Controller.php';
 	
 	$userid		= $_GET['user_id'];
 	$type		= $_GET['type'];
@@ -10,10 +11,18 @@
 	$fname		= $_POST['fname'];
 	$lname		= $_POST['lname'];
 	$gender		= $_POST['gender'];
+	$squestion	= $_POST['squestion'];
+	$sanswer	= $_POST['sanswer'];
 	
 	$uc			= new UserController();
+	$sc 		= new SecurityController();
+	$securityRecord = $sc->getSecurityRecord($userid);
 	$uc->updateUser($userid, $uname, $password, $fname, $lname, $gender);
-	
+	if(sizeof($securityRecord) == 1){
+		$sc->updateSecurityQuestion($squestion, $sanswer, $userid);
+	} else {
+		$sc->setSecurityQuestion($squestion, $sanswer, $userid);
+	}
 	if($type == 0) $_SESSION['uname'] = $uname;
 	
 	header("Location: edit-account.php?user_id={$userid}&f=1");
